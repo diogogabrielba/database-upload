60f020f501f38cb81b1b3de43c77a661f3f1f915
Diogo Gabriel Barbosa <diogo.gabriel259@gmail.com>
2020-07-27 15:27:35 -0300

    CRUD de transactions e upload de arquivos CSV

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,10 +1,58 @@
 // import AppError from '../errors/AppError';
+import { getCustomRepository, getRepository } from 'typeorm';
+import TransatcionRepository from '../repositories/TransactionsRepository';
 
 import Transaction from '../models/Transaction';
+import Category from '../models/Category';
+import AppError from '../errors/AppError';
+
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+  category: string;
+}
 
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
-    // TODO
+  public async execute({
+    title,
+    value,
+    type,
+    category,
+  }: Request): Promise<Transaction> {
+    const transatcionRepository = getCustomRepository(TransatcionRepository);
+    const categoryRepository = getRepository(Category);
+
+    const { total } = await transatcionRepository.getBalance();
+
+    if (type === 'outcome' && total < value) {
+      throw new AppError('You do not have enough balance');
+    }
+
+    let transectionCategory = await categoryRepository.findOne({
+      where: {
+        title: category,
+      },
+    });
+
+    if (!transectionCategory) {
+      transectionCategory = categoryRepository.create({
+        title: category,
+      });
+
+      await categoryRepository.save(transectionCategory);
+    }
+
+    const transaction = transatcionRepository.create({
+      title,
+      value,
+      type,
+      category: transectionCategory,
+    });
+
+    await transatcionRepository.save(transaction);
+
+    return transaction;
   }
 }
 
